4c0d12290bb47ffeda7bd9df3f71c990fee875a5
hongethan <614290813@qq.com>
2020-03-23 11:05:58 +0800

    commit new http request

--- bot.js
+++ bot.js
@@ -47,6 +47,31 @@ class EchoBot extends ActivityHandler {
 }
   
 async function requestRemoteByGetUser(url, user) {
+    const options = {
+      hostname: snxHost,
+      port: 443,
+      path: url,
+      method: 'GET',
+      headers: {
+        'user': crypto.createHash('sha1').update(user).digest('base64')
+      }
+    };
+    const request = https.get(options, res => {      
+      res.setEncoding('utf8');
+      let body = '';
+      res.on('data', data => {
+        body += data;
+      });
+      res.on('end', () => {
+        //resolve(body);    
+      });
+    });
+    
+    request.on('error', function(e) {
+      console.log('problem with request: ' + e.message);
+    });
+
+    request.end(); 
 }
 
 module.exports.EchoBot = EchoBot;
