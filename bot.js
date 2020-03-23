eea50198bc8dd1852a258fc67456d3e9cf391169
hongethan <614290813@qq.com>
2020-03-23 08:44:36 +0800

    commit vendor V0

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ class EchoBot extends ActivityHandler {
         super();
         // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
         this.onMessage(async (context, next) => {
-            await context.sendActivity(`You said '${ context.activity.text }'`);
+            await context.sendActivity(`You said '${ queryVendorInfo(context) }'`);
 
             // By calling next() you ensure that the next BotHandler is run.
             await next();
@@ -18,7 +18,7 @@ class EchoBot extends ActivityHandler {
             const membersAdded = context.activity.membersAdded;
             for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
                 if (membersAdded[cnt].id !== context.activity.recipient.id) {
-                    await context.sendActivity('Hello and welcome and Beat you and begin it!');
+                    await context.sendActivity('Hello and welcome and Try Vendor V0!');
                 }
             }
             // By calling next() you ensure that the next BotHandler is run.
@@ -26,5 +26,82 @@ class EchoBot extends ActivityHandler {
         });
     }
 }
+
+
+function queryVendorInfo(agent) {
+	let vend_name_pp = agent.activity.text;
+	console.log('--------------search vend_name:' + vend_name_pp);
+	
+	let path = encodeURI('/gateway/p1-service?app_code=vendor-service&invoke_method=/api/vendor/vendorNamePattern/{patternName}/headers&paths={\"patternName\":\"'+ vend_name + '\"}\"');
+	console.log('--------------search Path:' + path);
+	console.log('--------------search User:' + user);
+	
+	return requestRemoteByGetUser(path, user).then(function(result){
+		let items=JSON.parse(result);
+		console.log('--------------Result :' + items.toString());
+		console.log('--------------Result is items PART :' + (items instanceof Array));		
+				
+		if(!items.hasOwnProperty('message')){
+		    return 'I am sorry, I cannot find any related information. ';
+		}
+		if(!items.message.hasOwnProperty('data')){
+		    return 'I am sorry, I cannot find any related information. ';
+		}
+		if(!items.message.data.hasOwnProperty('content')){
+		    return 'I am sorry, I cannot find any related information. ';
+		}
+
+		var array = [];
+		if(!(items.message.data.content instanceof Array)){
+			array.push(items.message.data.content);
+		}else{
+			array = items.message.data.content;
+		}
+		
+		var resultvendor = 'Vendor Information: ' + '  \n\t\r';
+		for(var pos=0; pos < array.length; pos++){
+			resultvendor = resultvendor + array[pos].vendNo + '---' + array[pos].vendName + '  \n\t\r';
+		}
+		
+		if(array.length < 1){
+			resultvendor = resultvendor + 'Not Found';
+		}
+		return resultvendor;
+	}).catch(function(error){
+	  return 'I am sorry, I cannot find any related information. ';
+	});	
+  }
+
+  
+function requestRemoteByGetUser(url, user) {
+    console.log('enter Remote Call By GET');
+    return new Promise((resolve, reject) => {    
+      var crypto = require('crypto');
+      const options = {
+        hostname: snxHost,
+        port: 443,
+        path: url,
+        method: 'GET',
+        headers: {
+          'user': crypto.createHash('sha1').update(user).digest('base64')
+        }
+      };
+      const request = https.get(options, res => {      
+        res.setEncoding('utf8');
+        let body = '';
+        res.on('data', data => {
+          body += data;
+        });
+        res.on('end', () => {
+          console.log("Pure Result is : "+body); 
+          resolve(body);
+          //body = JSON.parse(body);
+          //console.log("Json Result is : "+body);     
+        });
+      });
+      
+      request.on('error', (err) => reject(err));    
+    });
+  }
 
 module.exports.EchoBot = EchoBot;
